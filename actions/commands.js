2200bd870fba479dff3abf06326f79a2ecbba721
Aleksi Hirvensalo <aleksi.hirvensalo@aalto.fi>
2021-02-04 16:28:09 +0200

    Simple move command added.

--- actions/commands.js
+++ actions/commands.js
@@ -1,1 +1,15 @@
-//Basic commands for that bot can take. 
+//Basic commands for bot to take. 
+
+//Moves given bot to direction ('forward', 'back', 'left' or 'right')
+const move = (bot, direction) => {
+    bot.setControlState(direction, true)
+}
+
+const move1 = (bot) => {
+    bot.moveVehicle(0.0, 1.0)
+}
+
+module.exports = {
+    move, 
+    move1
+}
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const mineflayer = require('mineflayer')
 const mineflayerViewer = require('prismarine-viewer').mineflayer
+const pathfinder = require('mineflayer-pathfinder').pathfinder
+const Movements = require('mineflayer-pathfinder').Movements
+const {GoalBlock, GoalFollow} = require('mineflayer-pathfinder').goals
 
 const config = require('./utils/config')
+
+const command = require('./actions/commands')
 
 /*Create .env file and insert EMAIL="" and PASSWORD=""
 */
@@ -17,20 +22,29 @@ const bot = mineflayer.createBot({
     auth: 'mojang'
 })
 
+bot.loadPlugin(pathfinder)
+
 bot.once('spawn', () => {
     mineflayerViewer(bot, {port: 3000})
-
-    entity = bot.nearestEntity()
-    console.log(entity)
-    if(entity) {
+    bot.chat('moromoro')
         
-        const inter = setInterval(() => {
-            bot.attack(entity, true)
-        }, 50)
-    }
+    const ent = bot.nearestEntity()
+
+    const mcData = require('minecraft-data')(bot.version)
+
+    const defaultMove = new Movements(bot, mcData)
+
+    const p = ent.position
+
+    bot.pathfinder.setMovements(defaultMove)
+    bot.pathfinder.setGoal(GoalFollow(ent, 1))
 })
 
 bot.on('chat', (username, message) => {
     if(username === bot.username) return
     bot.chat(message)
 })
+
+module.exports = {
+    bot
+}
